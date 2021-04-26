40a1acf9f929f3f1922ad6818ec441cabe1af733
Bruno Silva <brunomnsilva@gmail.com>
2021-04-26 18:01:14 +0100

    Initial commit

---
+++ student.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "student.h"
+#include "input.h"
+
+Student studentCreate(char *name, char *number, int grade) {
+    Student student;
+
+    strcpy(student.name, name);
+    strcpy(student.number, number);
+    student.grade = grade;
+
+    return student;
+}
+
