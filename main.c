baee8ab3f6b4795dc744c3da130fdd5915281025
brunomnsilva <brunomnsilva@gmail.com>
2026-04-21 15:55:36 +0100

    Update to new upstream repository version

--- main.c
+++ main.c
@@ -1,11 +1,28 @@
 #include <stdio.h> 
 #include <stdlib.h>
 #include "input.h"
+#include "list.h"
 
 int main() {
+    int numbers[15] = {4, 19, 11, 8, 18, 15, 9, 13, 20, 1, 17, 7, 2};
 
-    /* Program code. */
-    printf("I do nothing at this moment.\n");
+    PtList l = listCreate();
+    printf("Instance address = %p\n", l);
+
+    printf("Adding elements to list...\n");
+    for(int i=0; i < 15; i++) {
+        listAdd(l, i, numbers[i]);
+    }
+
+    int size;
+    listSize(l, &size);
+
+    /* TODO: Print all elements inside 'l' */
+
+    printf("List contains %d elements.\n", size);
+
+    listDestroy(&l);
+    printf("Instance address = %p\n", l);
 
     return EXIT_SUCCESS;
 }
